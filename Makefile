07cb7bf3cd823f1b911b1e6985c2fefcb9e73bb5
aburdeni <>
2018-10-27 22:44:47 +0300

    start

---
+++ Makefile
@@ -0,0 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: aburdeni <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/10/27 18:17:42 by aburdeni          #+#    #+#              #
+#    Updated: 2018/10/27 18:17:45 by aburdeni         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = ft_ssl
+# ◦ open
+# ◦ close
+# ◦ read
+# ◦ write
+# ◦ malloc
+# ◦ free
+
+# You must submit a Makefile. The Makefile must contain the usual rules and compile
+# the project as necessary.
---
+++ src/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aburdeni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/27 19:26:37 by aburdeni          #+#    #+#             */
+/*   Updated: 2018/10/27 19:26:39 by aburdeni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+
+	}
+	else
+		// usage: ft_ssl command [command opts] [command args]
+
+/*
+	ft_ssl: Error: 'foobar' is an invalid command.
+	
+	Standard commands:
+	
+	Message Digest commands:
+	md5
+	sha256
+*/
+}
